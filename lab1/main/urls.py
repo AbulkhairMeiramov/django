97795a31bb967422a76d477b3193862d42fbae83
Abulkhair <abulkhairmeiramov@gmail.com>
2021-02-27 13:48:54 +0600

    Some fixes

--- lab1/main/urls.py
+++ lab1/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from main.views import task_incomplete_list,task_complete_list
 
 urlpatterns = [
-    path('', task_incomplete_list),
-    path('<int:pk>/completed', task_complete_list)
+    path('todos/', task_incomplete_list),
+    path('todos/<int:pk>/completed', task_complete_list)
 ]
